f24a080d41bebdff4850f993327893d06b0e758f
silentclowns <amy@ice3studio.com>
2014-02-21 20:41:21 +0800

    AppLifeBalance V0.1
    Initial Desktop Build
    Changes to come:
    Blog (to be discussed)
    Responsive/Mobile adjustments
    Retina images
    Contact form

---
+++ src/documents/scripts/main.js
@@ -0,0 +1,9 @@
+
+$(window).load(function() { // makes sure the whole site is loaded
+	//CSS Onload animations
+	$('.row').addClass('animated fadeIn');
+	$('.logo').addClass('animated fadeInLeft');
+	$('.jumbotron h1, .tagline, .social').addClass('animated fadeInDown');
+	$('.jumbotron h2').addClass('animated fadeInUp');
+})
+
